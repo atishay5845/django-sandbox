798d1ac6baca4a9909bd517bed97cddeab15dcd5
atishay5845 <atishaysodhiya5845@gmail.com>
2026-09-10 00:29:28 +0530

    completed testing of views in app

--- project-1/sandbox/sand/urls.py
+++ project-1/sandbox/sand/urls.py
@@ -14,12 +14,9 @@ Including another URLconf
     1. Import the include() function: from django.urls import include, path
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
-from django.contrib import admin
+# from django.contrib import admin - removed this in app
 from django.urls import path
 from . import views
 urlpatterns = [
-    path('admin/', admin.site.urls),
-    path('',view=views.home, name='home'), # name is optional para 
-    path('about/', views.about, name='about'),
-    
+    path('all_sand/', views.all_sand, name='all_sand'),
 ]
